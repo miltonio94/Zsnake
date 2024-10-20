98cc6c873b0e78cbc53d9d3ce7bdd72432f29dd1
M. <miltonio94@gmail.com>
2024-10-20 12:08:11 +0100

    Snake moves

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,101 @@
 const std = @import("std");
+const raylib = @import("raylib");
+
+const screenWidth = 1080;
+const screenHeight = 720;
+
+const Rectangle = raylib.Rectangle;
+const Direction = enum { up, down, left, right };
+const Snake = struct {
+    pub const sectionSize = 10;
+    const maxSize = (screenWidth / Snake.sectionSize) * (screenHeight / Snake.sectionSize);
+    const sectionGap = 0.01;
+    body: [maxSize]Rectangle = undefined,
+    length: u16 = 3,
+    direction: Direction = Direction.left,
+    speed: f16 = 0.8,
+    pub fn init() Snake {
+        var snake = Snake{};
+
+        for (&snake.body, 0..) |*section, idx| {
+            if (idx > snake.length) {
+                break;
+            }
+            if (idx == 0) {
+                section.* = Rectangle{ .x = screenWidth / 2, .y = screenHeight / 2, .width = sectionSize, .height = sectionSize };
+                continue;
+            }
+            const prevSection = &snake.body[idx - 1];
+            section.* = Rectangle{ .x = prevSection.*.x + @as(f32, @floatFromInt(sectionSize)) + @as(f32, @floatFromInt(snake.length)) + sectionGap, .y = prevSection.*.y, .width = sectionSize, .height = sectionSize };
+        }
+
+        return snake;
+    }
+
+    pub fn move(self: *Snake) void {
+        for (&self.*.body, 0..) |*section, idx| {
+            if (idx > self.length) {
+                break;
+            }
+            switch (self.direction) {
+                .up => {
+                    section.y = section.y - self.speed;
+                    if (section.y < 0) {
+                        section.y = screenHeight;
+                    }
+                },
+                .down => {
+                    section.y = section.y + self.speed;
+                    if (section.y > screenHeight) {
+                        section.y = 0;
+                    }
+                },
+                .left => {
+                    section.x = section.x - self.speed;
+                    if (section.x < 0.0) {
+                        section.x = screenWidth;
+                    }
+                },
+                .right => {
+                    section.x = section.x + self.speed;
+                    if (section.x > screenWidth) {
+                        section.x = 0;
+                    }
+                },
+            }
+        }
+    }
+
+    pub fn draw(self: Snake) void {
+        for (&self.body, 0..) |*section, idx| {
+            if (idx > self.length) {
+                break;
+            }
+            if (idx == 0) {
+                raylib.drawRectangleRec(section.*, raylib.Color.red);
+                continue;
+            }
+            raylib.drawRectangleRec(section.*, raylib.Color.black);
+        }
+    }
+};
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    raylib.initWindow(screenWidth, screenHeight, "Znake");
+    defer raylib.closeWindow();
+
+    var snake = Snake.init();
+
+    raylib.setTargetFPS(144);
+
+    while (!raylib.windowShouldClose()) {
+        Snake.move(&snake);
+
+        raylib.beginDrawing();
+        defer raylib.endDrawing();
+
+        raylib.clearBackground(raylib.Color.ray_white);
+
+        snake.draw();
+    }
 }
