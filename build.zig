470eeb9f9940d900fc9a556f10c80d5eead7fcab
M. <miltonio94@gmail.com>
2024-11-15 21:10:27 +0000

    Task Fix github actions

--- build.zig
+++ build.zig
@@ -1,15 +1,22 @@
 const std = @import("std");
 const rlz = @import("raylib-zig");
 
+const LinuxDisplayBackEnd = enum {
+    x11,
+    wayland,
+};
+
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    const backend = b.option(LinuxDisplayBackEnd, "linux_display_backend", "Which linux display backend to use") orelse .wayland;
 
     const raylib_dep = b.dependency("raylib-zig", .{
         .target = target,
         .optimize = optimize,
         .opengl_version = rlz.OpenglVersion.gl_4_3,
-        .linux_display_backend = rlz.LinuxDisplayBackend.Wayland,
+        .linux_display_backend = if (backend == .wayland) rlz.LinuxDisplayBackend.Wayland else rlz.LinuxDisplayBackend.X11,
         .platform = .sdl,
     });
 
